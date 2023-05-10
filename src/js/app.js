70eeb81526f8b950cf1288be9a8edec7d57bad34
BatyrDaurov <118624947+BatyrDaurov@users.noreply.github.com>
2023-05-11 00:59:25 +0300

    delete .env

--- src/js/app.js
+++ src/js/app.js
@@ -6,8 +6,7 @@ import { chat } from './modules/chat.js';
 import { boardListener } from './modules/board.js';
 
 $(function () {
-  // wss://game-backend-hhff.onrender.com/ws/tictactoe
-  const ws = new WebSocket('ws://localhost:9999/ws/tictactoe');
+  const ws = new WebSocket('wss://game-backend-hhff.onrender.com/ws/tictactoe');
 
   /* ==== Game Listener ==== */
   boardListener(ws);
